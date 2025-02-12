899035ca686c86e3317391202619bcf97c9390a5
pavlokislov <kislovpasha@gmail.com>
2025-02-12 16:52:39 +0200

    add missing micro-visualizer files

---
+++ visualizer-service/src/main/java/org/pkislov/visualizerservice/VisualizerServiceApplication.java
@@ -0,0 +1,13 @@
+package org.pkislov.visualizerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualizerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisualizerServiceApplication.class, args);
+    }
+
+}
---
+++ visualizer-service/src/test/java/org/pkislov/visualizerservice/VisualizerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pkislov.visualizerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualizerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
